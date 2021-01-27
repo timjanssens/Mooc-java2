fd9223b8415f26e6288cf8c01bb58d353d1952ac
Tim Janssens <47323747+timjanssens@users.noreply.github.com>
2021-01-27 18:41:57 +0100

    Part08_02.AverageOfPositiveNumbers Recap

--- mooc-java-programming-ii/part08-Part08_02.AverageOfPositiveNumbers/src/main/java/AverageOfPositiveNumbers.java
+++ mooc-java-programming-ii/part08-Part08_02.AverageOfPositiveNumbers/src/main/java/AverageOfPositiveNumbers.java
@@ -6,5 +6,29 @@ public class AverageOfPositiveNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int sum = 0;
+        int totalNumbers = 0;
+        
+        while (true) {
+
+            int num = Integer.valueOf(scanner.nextLine());
+
+            if (num == 0){
+                if (totalNumbers > 0) {
+                    System.out.println(sum / (double)totalNumbers);
+                } else {
+                    System.out.println("Cannot calculate the average");
+                }
+                
+                break;
+            }
+            
+            if (num > 0){
+                sum += num;
+                totalNumbers++;
+            }
+            
+        }
+
     }
 }

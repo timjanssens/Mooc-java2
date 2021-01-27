226cc2be233cb90b077deac9ef9e4cdc3abedf91
Tim Janssens <47323747+timjanssens@users.noreply.github.com>
2021-01-27 17:59:06 +0100

    Add files

---
+++ mooc-java-programming-ii/part08-Part08_01.Cubes/src/main/java/Cubes.java
@@ -0,0 +1,25 @@
+
+import java.util.Scanner;
+
+public class Cubes {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        while(true){
+            
+            String number = scanner.nextLine();
+            
+            if (number.equals("end")) {
+                break;
+            }
+            
+            int num = Integer.valueOf(number);
+            
+            int cube = num * num * num;
+                    
+            System.out.println(cube);
+        
+        }
+    }
+}

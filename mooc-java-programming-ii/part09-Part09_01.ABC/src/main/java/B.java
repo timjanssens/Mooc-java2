149df50ccd88f451cd8ec9e0b5db06eecf24a945
Tim Janssens <47323747+timjanssens@users.noreply.github.com>
2021-02-01 17:37:13 +0100

    Part09_01.ABC Learn inheritance

---
+++ mooc-java-programming-ii/part09-Part09_01.ABC/src/main/java/B.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author timja
+ */
+public class B extends A{
+    
+        public void b(){
+            super.a();
+        System.out.println("B");
+    }
+        
+}
---
+++ mooc-java-programming-ii/part09-Part09_01.ABC/src/main/java/C.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author timja
+ */
+public class C extends B{
+    
+        public void c(){
+            super.b();
+        System.out.println("C");
+    }
+        
+}
